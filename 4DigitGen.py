486609b33dfd64bd1d2986eacd8a2a37224e0a9d
Joe Hollon <joehollon@Joes-Air.attlocal.net>
2019-10-11 23:02:16 -0400

    first commit

---
+++ 4DigitGen.py
@@ -0,0 +1,44 @@
+import hashlib
+
+open_file = open("numbers.txt", "r")
+stri= ""
+sum = 0
+for line in open_file:
+    stri+=line 
+numberList = stri.split()
+open_file.close()
+
+ade = "fe3ee0fed08037dfbb56a063d4674734"
+christian = "9a982e05c8a6a7b33d61fca3aa1af17c"
+elyse = "93ccd950dc65bdda2d7d72458f6fbe78"
+jenny = "834c834324c7319ffee40e6c8b7ad774"
+kristy = "677b60f801a3d18969c23425cb19ce4a"
+
+prefixString = "FLAG-HQNT-"
+passwordList = []
+
+for numbers in numberList:
+    passwordList.append(prefixString + numbers)
+    
+#print(passwordList)    
+
+for p in passwordList:
+    encrypted = hashlib.md5(p.encode('utf-8')).hexdigest()
+        
+    if encrypted == christian:
+        print("christian = " + p)  
+        
+    if encrypted == elyse:
+        print("elyse = " + p)
+        
+    if encrypted == kristy:
+        print("kristy = " + p)  
+        
+    if encrypted == ade:
+        print("ade = " + p) 
+        
+    if encrypted == jenny:
+        print("jenny = " + p)                   
+    
+    
+
